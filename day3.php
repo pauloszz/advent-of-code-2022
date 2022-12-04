4134f625c3999cd098568e4c13001f07c8f72a25
Paul Chevallier <paul@chevallier.nl>
2022-12-04 14:13:22 +0100

    day 3

---
+++ day3.php
@@ -0,0 +1,18 @@
+<?php
+
+$data = nl2br(file_get_contents('data/day2.txt'));
+
+
+
+echo "<br>";
+echo "<strong>Part 1 - Total points</strong><br>";
+
+echo "<br>";
+
+
+
+
+echo "<br>";
+echo "<strong>Part 2 - Total points</strong><br>";
+
+echo "<br>";
